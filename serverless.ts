27c461b0c90e6bdd80da642ec094e9c4687d41e9
fernandes-dev <eduardo.yugan@gmail.com>
2021-11-15 12:43:22 -0400

    feat: eslint + prettier

--- serverless.ts
+++ serverless.ts
@@ -1,23 +1,26 @@
-import type { AWS } from '@serverless/typescript';
-
-import hello from '@functions/hello';
+import hello from "@functions/hello";
+import type { AWS } from "@serverless/typescript";
 
 const serverlessConfiguration: AWS = {
-  service: 'serverless-desafio',
-  frameworkVersion: '2',
-  plugins: ['serverless-esbuild'],
+  service: "serverless-desafio",
+  frameworkVersion: "2",
+  plugins: [
+    "serverless-esbuild",
+    "serverless-dynamodb-local",
+    "serverless-offline",
+  ],
   provider: {
-    name: 'aws',
-    runtime: 'nodejs14.x',
+    name: "aws",
+    runtime: "nodejs14.x",
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
     },
     environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+      NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
     },
-    lambdaHashingVersion: '20201221',
+    lambdaHashingVersion: "20201221",
   },
   // import the function via paths
   functions: { hello },
@@ -27,11 +30,41 @@ const serverlessConfiguration: AWS = {
       bundle: true,
       minify: false,
       sourcemap: true,
-      exclude: ['aws-sdk'],
-      target: 'node14',
-      define: { 'require.resolve': undefined },
-      platform: 'node',
+      exclude: ["aws-sdk"],
+      target: "node14",
+      define: { "require.resolve": undefined },
+      platform: "node",
       concurrency: 10,
+    },
+    dynamodb: {
+      stages: ["dev", "local"],
+      start: {
+        port: 8000,
+        inMemory: true,
+        migrate: true,
+      },
+    },
+  },
+  resources: {
+    Resources: {
+      dbTodos: {
+        Type: "AWS::DynamoDB::Table",
+        Properties: {
+          ProvisionedThroughput: {
+            ReadCapacityUnits: 5,
+            WriteCapacityUnits: 5,
+          },
+          TableName: "todos",
+          AttributeDefinitions: {
+            AttributeName: "id",
+            AttributeType: "S",
+          },
+          KeySchema: {
+            AttributeName: "id",
+            KeyType: "HASH",
+          },
+        },
+      },
     },
   },
 };
